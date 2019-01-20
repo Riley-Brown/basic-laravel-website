0c76b7fa9be3641d08703534b654a91927f32481
Riley-Brown <riley@riley.gg>
2019-01-20 00:34:20 -0500

    Added messages view to loop through messages and display on message route

---
+++ resources/views/messages.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>Messages</h1>
+
+@if(count($messages) > 0)
+
+@foreach($messages as $message)
+<ul class="list-group mb-4">
+    <li class="list-group-item">
+        Name: {{$message->name}}
+    </li>
+    <li class="list-group-item">
+        Email: {{$message->email}}
+    </li>
+    <li class="list-group-item">
+        Message: {{$message->message}}
+    </li>
+</ul>
+@endforeach
+@endif
+
+@endsection
+
+@section('sidebar')
+@parent
+<p>this is appeneded to the side bar</p>
+@endsection
