3e0f7a1903f4a53c663540d803ad271a57e9b823
Riley-Brown <riley@riley.gg>
2019-01-19 23:31:43 -0500

    created database tables and migrated with php artisan, added flash messages for successful contact form submission. Connected message controller to mySQL DB

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Message;
 
 class MessagesController extends Controller
 {
@@ -11,6 +12,17 @@ class MessagesController extends Controller
             'name' => 'required',
             'email' => 'required'
         ]);
-        return 'success';
+        
+        // create message 
+        $message = new Message;
+        $message->name = $request->input('name');
+        $message->email = $request->input('email');
+        $message->message = $request->input('message');
+        
+        // save message to DB
+        $message->save();
+
+        // redirect after submit
+        return redirect('/')->with('success', 'Message Sent');
     }
 }
